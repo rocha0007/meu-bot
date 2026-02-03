2ac80ac47e1417d42fc165bfe9a9ad83c82057f2
rocha0007 <barretorochawillian@gmail.com>
2026-02-03 19:06:51 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def keep_alive():
     t.start()
 
 # --- CONFIGURAÇÃO ---
-TOKEN = 'MTQ2ODE2ODU3NDQ1NDcyNjc1MA.GLMBy4.JPBaLO7H51aDF8ki6jolz-ubxCtIkJikOjH0Hg'
+TOKEN = 'MTQ2ODE2ODU3NDQ1NDcyNjc1MA.GklqZE.EB9wwR06cfA-Didy7-O3xPzwB6N-3V56utY-bw'
 COR_ROXA = 0x8e44ad 
 
 intents = discord.Intents.default()
